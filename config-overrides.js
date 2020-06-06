716cb290489ee2fe0f1130e49bf958c49f1d4be8
shihongxia <1054689185@qq.com>
2020-06-06 22:20:35 +0800

    feat: 添加装饰器模式写法

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { addDecoratorsLegacy, override, fixBabelImports, addLessLoader } = require('customize-cra');
 const modifyVars = require('./theme')
 module.exports = override(
+  addDecoratorsLegacy(),
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { Button } from 'antd';
+import {Component} from 'react'
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+
+
+const testHOC = (WrappedComponent)=>{
+  return class HOCComponent extends Component{
+    render(){
+      return (
+        <>
+          <WrappedComponent />
+          <div>这是装饰器模式写法</div>
+        </>
+      )
+    }
+}
+}
+
+@testHOC
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">xxx</Button>
+      </div>
+    )
+  }
+}
+
 
 export default App;
