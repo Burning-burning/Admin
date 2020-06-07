973cea38bc4a2aa2b3bfdbe4a320696006e1f7b1
shihongxia <1054689185@qq.com>
2020-06-07 11:29:51 +0800

    feat: 配置页面以及外层路由

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {mainRouter} from './routes/index'
+import {BrowserRouter, Switch, Route,Redirect} from 'react-router-dom'
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/admin' render={(routerProps)=>{
+        return <App />
+      }} />
+      {mainRouter.map(route=>{
+        return <Route path={route.pathname} component={route.component} />
+      })}
+      <Redirect to='/admin' from ='/' exact />
+      <Redirect to='/404' />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 )
---
+++ src/routes/index.jsx
@@ -0,0 +1,29 @@
+import Login from '../containers/Login'
+import NotFound from '../containers/NotFound'
+import ArticleList from '../containers/Articles'
+import ArticleEdit from '../containers/Articles/edit'
+import Settings from '../containers/Settings'
+import DashBoard from '../containers/DashBoard'
+
+export const mainRouter  = [{
+  pathname: '/login',
+  component: Login
+},{
+  pathname: '/404',
+  component:NotFound
+}]
+
+export const adminRouter =[{
+  pathname: '/admin/dashboard',
+  component: DashBoard
+},{
+  pathname: '/admin/settings',
+  component: Settings
+},{
+  pathname: '/admin/article',
+  component: ArticleList,
+  exact:true
+},{
+  pathname: '/admin/article/edit:id',
+  component: ArticleEdit
+}]
